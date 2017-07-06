58cd5ac3eb1cfd806478c3363bdc9dad31d6c16d
5nilha <bwtdirect@gmail.com>
2017-07-06 00:17:43 -0400

    Add Categories to Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705220732) do
+ActiveRecord::Schema.define(version: 20170706034144) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170705220732) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
+    t.integer "category_id"
   end
 
 end
